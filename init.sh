b6ba68571e2e168939d813943b12dfb259a75d6a
rnebular <jtbyrum@gmail.com>
2021-11-08 22:21:10 -0800

    functional backup/restore scripts, service file, config file and user data init script

--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
 # startup user data for ec2 instance.
 sudo yum update -y
 
 # Install SteamCMD pre-reqs
+sleep 10
 sudo yum install -y glibc.i686 libstdc++48.i686
 
 # create location for games
@@ -24,15 +26,21 @@ export SteamAppId=294420
 # Launch SteamCMD and update 7 Days to Die (gameid=294420 )
 sudo ./steamcmd.sh +login anonymous +force_install_dir /games/7days2die +app_update $SteamAppId validate +quit
 
-# saves are:
-# /root/.local/share/7DaysToDie/Saves/Navezgane/Warm Office Server/
-# copy Save files from s3 (commented out)
-aws s3 cp s3://8dot3/7days/Saves/* '/root/.local/share/7DaysToDie/Saves/Navezgane/My Game/' --recursive --force
-
 # copy server config file from s3
 cd /games/7days2die
 sudo aws s3 cp s3://8dot3/7days/serverconfig.xml .
+sudo aws s3 cp s3://8dot3/7days/7days.service .
 chmod +x ./serverconfig.xml
+chmod +x ./7days.service
 
-cd /games/7days2die
-sudo ./startserver.sh -configfile=serverconfig.xml
+sudo cp 7days.service /etc/systemd/system
+sudo systemctl start 7days
+
+# Restore saves:
+# (location on instance) /root/.local/share/7DaysToDie/Saves/Navezgane/My Game/
+# copy Save files from s3 (commented out)
+# aws s3 cp "s3://8dot3/7days/Saves/" '/root/.local/share/7DaysToDie/Saves/Navezgane/My Game/' --recursive --force
+
+# manual start game at CLI
+# cd /games/7days2die
+# sudo ./startserver.sh -configfile=serverconfig.xml
