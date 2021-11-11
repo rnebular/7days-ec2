b3f1aaf59763dd5df376ce41321e32e26e65a550
rnebular <jtbyrum@gmail.com>
2021-11-11 15:15:12 -0800

    updated backup and restore scripts

--- backup-to-s3.sh
+++ backup-to-s3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /root/.local/share/7DaysToDie/Saves/Navezgane/
-aws s3 cp "Warm Office Server" "s3://8dot3/7days/Saves/Warm Office Server" --recursive
+aws s3 cp "My Game" "s3://8dot3/7days/Saves/My Game" --recursive
--- restore-from-s3.sh
+++ restore-from-s3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Restoring worlds from s3
-cd ~/.config/unity3d/IronGate/Valheim/worlds
-aws s3 cp s3://8dot3/valheim/worlds/ . --recursive
+cd /root/.local/share/7DaysToDie/Saves/Navezgane/
+aws s3 cp "s3://8dot3/7days/Saves/My Game" . --recursive
