a2c80c57f001658c148657ceb0f46c04946ccdb9
rnebular <jtbyrum@gmail.com>
2021-11-13 10:27:51 -0800

    adding updates to s3 scripting

--- backup-to-s3.sh
+++ backup-to-s3.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
+
+# backup from local to s3, and then from s3 timestamp to s3 latest
 cd /root/.local/share/7DaysToDie/Saves/Navezgane/
-aws s3 cp "My Game" "s3://8dot3/7days/Saves/My Game" --recursive
+aws s3 cp "My Game" "s3://8dot3/7days/Saves/My Game/$TIMESTAMP" --recursive
+aws s3 cp "s3://8dot3/7days/Saves/My Game/$TIMESTAMP" "s3://8dot3/7days/Saves/My Game/latest" --recursive
--- restore-from-s3.sh
+++ restore-from-s3.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-# Restoring worlds from s3
+# Restoring worlds from s3 latest, will need to make this script do TIMESTAMP based lookups with input?
+TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
+
 cd /root/.local/share/7DaysToDie/Saves/Navezgane/
-aws s3 cp "s3://8dot3/7days/Saves/My Game" . --recursive
+aws s3 cp "s3://8dot3/7days/Saves/My Game/latest" . --recursive
